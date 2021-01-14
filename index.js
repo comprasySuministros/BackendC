319f326eb5f8fc7921fedf1990b05f2ca28f0366
cesar <csgalindos@hotmail.com>
2021-01-14 15:41:09 -0500

    [updated] add cors

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express')
 const app = express()
-// const cors = require('cors')
+const cors = require('cors')
 const {config} = require('./config/index')
 const articlesServices = require('./routes/articles')
-const {logError,wrapError, handlerError} = require('./utils/middleware/errorHandler')
-const notfoundhandler = require('./utils/middleware/notFoundHandler')
+// const {logError,wrapError, handlerError} = require('./utils/middleware/errorHandler')
+// const notfoundhandler = require('./utils/middleware/notFoundHandler')
 
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
 articlesServices(app)
-app.use(notfoundhandler)
+// app.use(notfoundhandler)
 
-app.use(logError)
-app.use(wrapError)
-app.use(handlerError)
+// app.use(logError)
+// app.use(wrapError)
+// app.use(handlerError)
 
 app.listen(config.port, () =>{
   console.log(`server listening in port http://localhost:${config.port}`);
